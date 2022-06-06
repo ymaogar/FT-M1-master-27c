a64780a8832be39683f3ac38ee3de498399bf6a2
ymaogar <ymauriciogarcia@gmail.com>
2022-06-06 17:03:48 -0500

    01 terminado

---
+++ 01-IntroToCS/homework/homework.js
@@ -0,0 +1,27 @@
+'use strict'
+
+function BinarioADecimal(num) {
+  var decimal = 0;
+  for (let index = 0; index < num.length; index++) {
+  decimal = decimal+num[index]*2**(num.length-1-index);
+
+  }
+  return decimal;
+
+}
+
+function DecimalABinario(num) {
+  var array =[];
+  while(num>0){
+    array.unshift(num%2);
+    num = Math.floor(num/2);
+  }
+return array.join('');
+
+}
+
+
+module.exports = {
+  BinarioADecimal,
+  DecimalABinario,
+}
